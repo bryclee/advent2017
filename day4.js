3fcf12bac3f14e489a00f4865dad180674934c67
Bryan Lee <pygmize.me@gmail.com>
2017-12-14 10:10:47 -0800

    day 3 and 4

---
+++ day4.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const input = fs.readFileSync('./day4input.txt').toString();
+const passphrases = input.split('\n').filter(x => x);
+
+function validate(fn, str) {
+    const words = str.split(' ');
+    const dict = {};
+
+    return !words.some(word => {
+        const key = fn(word);
+
+        if (dict[key]) return true;
+
+        dict[key] = (dict[key] || 0) + 1;
+        return false;
+    });
+}
+
+const validateDuplicates = validate.bind(null, x => x);
+const validateAnagrams = validate.bind(null, x => x.split('').sort().join(''));
+
+function test1(passphrases) {
+    const result = passphrases.reduce((sum, n) => {
+        if (validateDuplicates(n)) {
+            sum++;
+        }
+
+        return sum;
+    }, 0);
+
+    console.log('Test 1:', result);
+}
+
+function test2(passphrases) {
+    const result = passphrases.reduce((sum, n) => {
+        if (validateDuplicates(n) && validateAnagrams(n)) {
+            sum++;
+        }
+
+        return sum;
+    }, 0);
+
+    console.log('Test 2:', result);
+}
+
+
+test1(passphrases);
+test2(passphrases);
---
+++ day5.js
@@ -0,0 +1,49 @@
+const input = require('fs').readFileSync('./day5input.txt').toString();
+const instructions =
+    input.split('\n')
+        .filter(x => x)
+        .map(x => Number(x));
+
+function test1(inst) {
+    inst = inst.slice();
+    let position = 0;
+    let steps = 0;
+
+    while(position in inst) {
+        const old = position;
+
+        position += inst[position];
+        inst[old]++;
+        steps++;
+    }
+
+    return steps;
+}
+
+function test2(inst) {
+    inst = inst.slice();
+    let position = 0;
+    let steps = 0;
+
+    while (position in inst) {
+        const jump = inst[position];
+        const old = position;
+
+        position += inst[position];
+        if (jump >= 3) {
+            inst[old]--;
+        } else {
+            inst[old]++;
+        }
+        steps++;
+    }
+
+    return steps;
+}
+
+const sampleInstructions = [0,3,0,1,-3];
+
+console.log('test 1 with sample:', test1(sampleInstructions));
+console.log('test 1:', test1(instructions));
+console.log('test 2 with sample:', test2(sampleInstructions));
+console.log('test 2:', test2(instructions));
